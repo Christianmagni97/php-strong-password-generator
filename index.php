4b63b1bd3c2c016f9d1ce51852429176c07a6bb3
Christianmagni97 <christianmagni28@gmail.com>
2024-06-30 21:31:56 +0200

    aggiunto header con a href di login, logout ecc

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ $utenti =[
    
 ];
 session_start();
+var_dump($_SESSION);
 ?>
 
 <!DOCTYPE html>
@@ -31,10 +32,16 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title></title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <header>
+        <section class='bottoni'>
+           <a class='button-menu' href='index.php'>Home</a>
+           <a class='button-menu' href='login.php'>Accedi!</a> 
+           <a class='button-menu' href='logout.php'>Disconettiti!</a>
+        </section>   
+    </header>
     
 </body>
 </html>
