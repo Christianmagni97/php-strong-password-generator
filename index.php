b2767fb90c99c75d81673009dec9d82d9578d11a
Christianmagni97 <christianmagni28@gmail.com>
2024-06-30 19:18:23 +0200

    aggiunto utenti per login

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+$utenti =[
+    [
+        'username' => 'geralt_di-rivia',
+        'password' => 'yennefer'
+
+    ],
+    [
+        'username' => 'nick_valentine',
+        'password' => 'dogmeat'
+
+    ],
+    [
+        'username' => 'panam-palmer',
+        'password' => 'aldecaldos'
+
+    ],
+    [
+        'username' => 'kassandra',
+        'password' => 'setta-di-cosmos'
+
+    ],
+   
+];
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
