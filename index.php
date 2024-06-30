99d8846cf98dd4dd1604c40026bbff4ef4183886
Christianmagni97 <christianmagni28@gmail.com>
2024-06-30 23:34:25 +0200

    base di ricerca username

--- index.php
+++ index.php
@@ -1,47 +1,16 @@
+
 <?php
-
-$utenti =[
-    [
-        'username' => 'geralt_di-rivia',
-        'password' => 'yennefer'
-
-    ],
-    [
-        'username' => 'nick_valentine',
-        'password' => 'dogmeat'
-
-    ],
-    [
-        'username' => 'panam-palmer',
-        'password' => 'aldecaldos'
-
-    ],
-    [
-        'username' => 'kassandra',
-        'password' => 'setta-di-cosmos'
-
-    ],
-   
-];
 session_start();
-var_dump($_SESSION);
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <section class='bottoni'>
-           <a class='button-menu' href='index.php'>Home</a>
-           <a class='button-menu' href='login.php'>Accedi!</a> 
-           <a class='button-menu' href='logout.php'>Disconettiti!</a>
-        </section>   
-    </header>
-    
-</body>
-</html>
+if(isset($_GET['username'])){
+    $_SESSION['name'] = $_GET['username'];?>
+    <h1>
+        <?php echo 'Benvenuto '. $_SESSION['name']?>
+    </h1>
+<?php } elseif(isset($_SESSION['name'])) {?>
+    <h1>
+        <?php echo 'Benvenuto '. $_SESSION['name']?>
+    </h1>
+<?php } else{?>
+    <h1>Non hai accesso</h1>
+    <?php }?>
